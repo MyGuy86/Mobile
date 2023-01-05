d1dcf0806cf3afdfbbffff59c1ce5f876cd9e3c6
MyGuy86 <MyGuy86@users.noreply.github.com>
2023-01-04 22:50:03 -0500

    not working

--- app/src/main/java/com/example/numbereight/MainActivity2.java
+++ app/src/main/java/com/example/numbereight/MainActivity2.java
@@ -2,7 +2,11 @@ package com.example.numbereight;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -11,4 +15,15 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
     }
+
+    private class GetDataTask extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... voids) {
+            String response = "";
+            try {
+                URL url = new URL("https://api.example.com/endpoint");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection
+
+
+
 }
